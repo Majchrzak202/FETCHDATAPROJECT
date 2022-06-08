42eda46d177ec3d4ba919f4c466711eedb7b4c8c
MarcinMaster <m.luba505@gmail.com>
2022-06-08 08:44:40 +0200

    Added not working Post movie useFetch Hook solution - will wokr on it and solve it!

--- src/components/PostMovie.js
+++ src/components/PostMovie.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AddMovie from "./AddMovie";
+import useFetch from "./hooks/useFetch";
 
-const PostMovie = ({addMovieHandler}) => {
-    return (<div>
-        <AddMovie addMovieHandler={addMovieHandler}/>
-    </div>)
-}
+const PostMovie = ({ api }) => {
+  const [movies, setMovies] = useState("");
 
-export default PostMovie; 
+  const addMovieHandler = (movie) => {
+    setMovies(movie).then;
+    postMovies();
+  };
+
+
+
+  const { sendRequest: postMovies } = useFetch({
+    url: api.base,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: { movies },
+  }, addMovieHandler);
+
+  useEffect(() => {
+    postMovies();
+  }, [postMovies]);
+
+  return (
+    <div>
+      <AddMovie addMovieHandler={addMovieHandler} />
+    </div>
+  );
+};
+
+export default PostMovie;
